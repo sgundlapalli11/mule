9e2d86302fb5cb1d61336ca89f5f8411c4457c4c
kurtcarpenter <kurtcarpenter@hotmail.com>
2015-09-10 15:52:19 -0400

    it runs via shell

--- src/main/java/team/MainApp.java
+++ src/main/java/team/MainApp.java
@@ -1,18 +1,13 @@
-package team;
-
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 
 public class MainApp extends Application {
-
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
-
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(rootNode, 400, 200);
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 200);
 
         stage.setTitle("Hello JavaFX and Maven");
         stage.setScene(scene);
